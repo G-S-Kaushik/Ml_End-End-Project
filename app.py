324e98ef975ed2405443f75a67f1b6d5d542a867
Kaushik <g.skaushik41@gmail.com>
2024-01-27 13:11:31 +0530

    Model Training

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.mlproject.exception import CustomException
 import sys
 from src.mlproject.components.data_ingetion import DataIngetion,DataIngetionConfig
 from src.mlproject.components.data_transformation import DataTransformation,DataTransformationConfig
-
+from src.mlproject.components.model_trainer import ModelTrainer,ModelTrainerConfig
 
 
 
@@ -17,8 +17,12 @@ if __name__=="__main__":
         train_data_path,test_data_path=data_ingetion.initiate_data_ingetion()
 
         data_tranformation=DataTransformation()
-        data_tranformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_tranformation.initiate_data_transformation(train_data_path,test_data_path)
 
+        ##Model Training
+        model_trainer=ModelTrainer()
+        print(model_trainer.initate_model_trainer(train_arr,test_arr))
+    
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
